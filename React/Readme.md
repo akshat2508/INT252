94816a0682e76e040464df30f28cd076a9641707
Akshat-paul <akshatpaul2006@gmail.com>
2025-10-17 11:53:12 +0530

    COMMIT: Dated : 17/10/2025

--- React/Readme.md
+++ React/Readme.md
@@ -9,3 +9,6 @@ component - first letter capital
 rafce - react arrow function export component  - shortcut for component expo function 
 
 index.html > main.jsx > App.jsx > component
+
+
+DATED : 17/10/2025 {Array functions , map reduce , reverse , to lower case , to uppercase  , .from , new Set(nums)}
--- React/SecondClass/index.js
+++ React/SecondClass/index.js
@@ -1,5 +1,9 @@
 // let family = new Array("Akshat","manveer" , "Tanveer");
 
+
+
+
+
 // console.log(family);
 
 // let cousins = new Array("Sidhant" , "Mrinalini", "Mani");
@@ -93,16 +97,130 @@
 // console.log(expensive);
 
 
-let numbers = [2 ,3, 6, 8, 16,15 , 20];
-let final=[];
-numbers.forEach((a , b) => {
-    if(b % 2)
-    {
-        final.push(a * 2);
-    }
-    else{
-        final.push(a * 5);
-    }
-})
 
-console.log(final);
+
+// let numbers = [2 ,3, 6, 8, 16,15 , 20];
+// let final=[];
+// numbers.forEach((a , b) => {
+//     if(b % 2)
+//     {
+//         final.push(a * 2);
+//     }
+//     else{
+//         final.push(a * 5);
+//     }
+// })
+
+// console.log(final);
+
+
+
+// MAP Function TWO METHODS-----––––––--------–––––––––––––––––––––––––/
+
+// let nums = [3 ,4,5,6,18,12,5,12,16];
+// nums.map(work);
+
+// function work(x ,y)
+// {
+//     console.log(x , " : " , y);
+// }
+
+
+// SECOND METHOD ––––––––––––––––––––––––––
+// nums.map((x , y) => console.log(x , " : " , y));
+
+// nums.reverse();
+// console.log(nums);
+// console.log(nums.length);
+
+
+
+//STRING TOUPPERCASE AND TOLOWERCASE--–––––––––––––––––––––––––––––––––––––––––––––––––––/
+
+// let friends = ["Ram", "Shyam" ,"krishan", "Laxman"];
+// friends.map((x ,y) => console.log(x.toUpperCase()));
+// console.log(" ");
+// friends.map((x,y) => console.log(x.toLowerCase()));
+
+
+
+
+// let fruits = ['Mango','Apple' ,'Kiwi','papaya'];
+
+
+// let result= fruits.find(x => x == 'Apple');
+// console.log(result);
+
+//ARRAY.FROM()-––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––///
+
+// let nums = [3 ,7 ,48,87 , 29, 3 , 48 , 57 , 7, 48 ];
+// let unique = Array.from(new Set(nums)); 
+// let unique1 = new Set(nums); //RETURNS AN OBJECT
+// console.log(unique);
+
+
+
+
+// let str = "welcome to lpu";
+// console.log(Array.from(str));
+// let sum = 0;
+// for(i = 0 ; i< nums.length;i++)
+// {
+//     sum+=nums[i];
+// }
+// console.log(sum);
+
+// nums.forEach((x) => sum+=x);
+// console.log(sum);
+
+
+// nums.every((x)=> sum = sum+x);
+// console.log(sum);
+
+// REDUCE FUNCTION ––––––––––––––––––––––––––––––––––––––––––––––––––––––––÷
+
+// let nums = [3 ,7 ,48,87 , 29, 3 , 48 , 57 , 7, 48 ];
+
+// // let sum = nums.reduce((x, y) =>  x + y, 100);//(X is accumulator that carroes the value cumulative and y is the current value and 0 is the intial value)
+
+// // console.log(sum);
+
+// // SPREAD OPERATOR ... –––––––––––––––––––––––––––––––––––––––––––––––––––––––––÷÷÷
+
+// let friends = ["Ram" ,"Shyam", "Krishna"];
+// let family = ["Akshat" ,"Manveer" ,"Tanveer"];
+// // console.log(nums,friends);
+
+// console.log(...family, ...friends, ...nums);
+
+
+
+// let fruits = ["Mango", "Apple" ,
+//  "Guava",
+//  "kiwi"
+// ]
+
+// console.log(fruits);
+
+// let [a ,b, c ,d] = fruits;
+
+// console.log(a ,b, c,d);
+
+const form = document.getElementById("myForm");
+
+const nameIn = document.getElementById("name");
+
+const email = document.getElementById("email");
+const number = document.getElementById("number");
+const outputDiv = document.getElementById("output");
+const submit = document.getElementById("sub");
+form.addEventListener("submit", function(e) {
+    e.preventDefault(); 
+
+
+    outputDiv.innerHTML = `
+        <p>Name: ${nameIn.value}</p>
+        <p>Email: ${email.value}</p>
+        <p>Number: ${number.value}</p>
+    `;
+});
