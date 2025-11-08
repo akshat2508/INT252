cc6f5b622a5340cb16098d1b48b2656f06a4d3ce
Akshat-paul <akshatpaul2006@gmail.com>
2025-11-08 20:49:35 +0530

    8 Nov saturday commit

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,42 +1,16 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 const App = () => {
 
-  let [count , setCount]= useState(0);
+    let data = useRef();
 
-  function handleIncrement()
-  {
-    if(count  >= 10)
+    function handleClick()
     {
-        return;
-
+      
     }
-    setCount(count + 1);
-  }
-
-  function handleDecrement()
-  {
-    if(count === 0)
-    {
-      return
-    }
-    
-    setCount(count  - 1);
-  }
-  function handleReset()
-  {
-    
-    setCount(0);
-  }
   return (
     <>
-    <div className=' flex justify-evenly items-center mt-30'>
-      <button onClick={handleIncrement} className='bg-red-400 text-3xl w-40'> + </button>
-      <h1 className='p-3 m-3 text-3xl'>{count}</h1>
-      <button onClick={handleDecrement} className='bg-red-400 text-3xl w-40'> - </button>
-      <button onClick={handleReset} className='bg-green-400 text-3xl w-40'> reset </button>
-    </div>
-
+      <h1>hello</h1>
     </>
   )
 }
